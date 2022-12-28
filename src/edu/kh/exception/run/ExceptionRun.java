ae86cea4240e4de47df3cb1285f7ca5db0cd713e
seungmi <bbsm314@gmail.com>
2022-12-28 09:23:39 +0900

    First commit

---
+++ src/edu/kh/exception/run/ExceptionRun.java
@@ -0,0 +1,14 @@
+package edu.kh.exception.run;
+
+import edu.kh.exception.modle.service.ExceptionService;
+
+public class ExceptionRun {
+
+	public static void main(String[] args) {
+
+		ExceptionService es = new ExceptionService();
+		
+		es.ex3();
+	}
+
+}
